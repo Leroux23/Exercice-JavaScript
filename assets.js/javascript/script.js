5d8031e4cb8fad9c5036a9410d64968d2a0b8a1b
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-04 16:49:44 +0100

    Fonction carré avec prompt

--- assets.js/javascript/script.js
+++ assets.js/javascript/script.js
@@ -1,2 +1,6 @@
-var truc = calculerCarre(10);
-console.log(truc);
+var demande = prompt("Veuillez saisir une valeur");
+
+var bidule = calculerCarre(demande);
+
+alert(bidule);
+//alert(calculerCarre(demande));
