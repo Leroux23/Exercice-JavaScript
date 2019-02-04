097ea2d64270ea415a04a271c0723d7d7fcd3124
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-04 14:37:11 +0100

    Intégration d'un modulo

--- assets.js/javascript/script.js
+++ assets.js/javascript/script.js
@@ -1,7 +1,9 @@
 var resultat = "<ul>";
 for (var i = 0 ;i<= 100; i++) {
+  if (i % 2 == 0) {
 resultat +=  "<li>" + i + "</li>";
 //resultat = resultat + "<li>" + i + "</li>"
+}
 }
 resultat = resultat + "</ul>";
 document.write(resultat);
