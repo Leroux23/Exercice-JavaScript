e3ef763596b8ecbb4ef4477c4de0ad1a0c76bae3
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-04 16:15:17 +0100

    Création du fichier function

--- assets.js/javascript/script.js
+++ assets.js/javascript/script.js
@@ -4,11 +4,12 @@ for (var i = -12728 ;i<= 16856; i += 17) {
   // i += 10 -> i = i + 10
   if (i % 23 == 0) {
 resultat +=  "<li>" + i + "</li>";
-//resultat = resultat + "<li>" + i + "</li>"
+//resultat = resultat + "<li>" + i + "</li>";
 compteur ++
 }
 
 }
 resultat = resultat + "</ul>";
+// resultat + "</ul>";
 document.write(resultat);
 alert(compteur);
