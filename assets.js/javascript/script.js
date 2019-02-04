5f0acf61303e99fc2b0ac1cc0d34fb0560419f06
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-04 16:44:27 +0100

    Calcul fonction carré.

--- assets.js/javascript/script.js
+++ assets.js/javascript/script.js
@@ -1,15 +1,2 @@
-var resultat = "<ul>";
-var compteur = 0;
-for (var i = -12728 ;i<= 16856; i += 17) {
-  // i += 10 -> i = i + 10
-  if (i % 23 == 0) {
-resultat +=  "<li>" + i + "</li>";
-//resultat = resultat + "<li>" + i + "</li>";
-compteur ++
-}
-
-}
-resultat = resultat + "</ul>";
-// resultat + "</ul>";
-document.write(resultat);
-alert(compteur);
+var truc = calculerCarre(10);
+console.log(truc);
